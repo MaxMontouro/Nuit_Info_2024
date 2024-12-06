20002e0c2531b7004f84eb28dc86a11fc8dc711d
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2024-12-06 03:56:01 +0100

    Gestion des mouvements du player avec les levels et clique sur level

--- src/app/components/world/level/level.component.ts
+++ src/app/components/world/level/level.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Level, LevelStatus } from '../../../models/level.model';
+import { LevelService } from '../../services/level.service';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-level',
@@ -13,12 +15,24 @@ export class LevelComponent {
   @Output() start = new EventEmitter<number>();
   @Output() completed = new EventEmitter<number>();
 
+  constructor(private levelService: LevelService, private playerService: PlayerService) {}
+
   getPlatformColor(): string {
     return this.level.background;
   }
 
-  launchLevel() {
+  clickLevel(): void {
+    if(this.playerService.getPlayer().numNiveauActuel === this.level.num) {
       this.start.emit(this.level.num);
+    }
+    else {
+      if(this.playerService.getPlayer().numNiveauxPasses.includes(this.level.num)) {
+        this.levelService.moveToNextLevel();
+      }
+      else {
+        this.levelService.moveToPreviousLevel();
+      }
+    }
   }
 
   isValidated(): boolean {
